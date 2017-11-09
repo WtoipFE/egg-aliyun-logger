3f15dce064cc93c762595673f0e73c3069a6fc28
wanghaikun <wanghaikun@wtoip.com>
2017-11-09 17:54:51 +0800

    feat: 将公共的日志封装到logutil函数中

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ module.exports = app => {
   // 注入公共函数
   app.aliyunLog = LogUtil.bind(this, app);
   // 实例化logger通道
-  if (app.config.aliyunLogger.app) app.addSingleton('aliyunLogger', createClient);
+  // if (app.config.aliyunLogger.app) app.addSingleton('aliyunLogger', createClient);
   // 插入中间件
   if (app.config.aliyunLogger.middleware)  app.config.coreMiddleware.push('aliyunLogger');
 }
