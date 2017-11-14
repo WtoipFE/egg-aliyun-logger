9f0a78acb1dd2f3b658e57efeb22e68f0a2a33e8
wanghaikun <wanghaikun@wtoip.com>
2017-11-14 09:38:20 +0800

    feat: 格式优化

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const AliyunLog = require('./lib/AliyunLog')
 
 module.exports = app => {
   // 注入公共函数
-  app.aliyunLog = AliyunLog.bind(this, app);
+  app.aliyunLog = AliyunLog.bind(this, app)
   // 实例化logger通道
-  // if (app.config.aliyunLogger.app) app.addSingleton('aliyunLogger', createClient);
+  // if (app.config.aliyunLogger.app) app.addSingleton('aliyunLogger', createClient)
   // 插入中间件
-  if (app.config.aliyunLogger.middleware)  app.config.coreMiddleware.push('aliyunLogger');
+  if (app.config.aliyunLogger.middleware)  app.config.coreMiddleware.push('aliyunLogger')
 }
 
 function createClient(config, app) {
-  const client = new AliyunTransport(config);
-  app.logger.set(config.clientId, client);
-  return client;
+  const client = new AliyunTransport(config)
+  app.logger.set(config.clientId, client)
+  return client
 }
--- app/middleware/aliyunLogger.js
+++ app/middleware/aliyunLogger.js
@@ -2,7 +2,7 @@ const LogUtil = require('../../lib/LogUtil')
 
 module.exports = (options, app) => {
   return function* (next) {
-    yield next;
+    yield next
     
     LogUtil(this.app, this)
   }
